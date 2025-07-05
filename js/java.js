df61d3472f001a9d09eb731fda0d97f71803e144
shreensamhi <shreensamhi01@gmail.com>
2025-07-05 21:50:13 +0300

    update

---
+++ js/java.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.animation', {
+      strings: ["Developer ","Designer "],
+     typeSpeed: 50,           // Word appears instantly
+    backSpeed: 80,           // Word disappears instantly
+    backDelay: 1500,        // Stay for 1.5s before switching
+    startDelay: 300,        // Delay before typing starts
+    loop: true,             // Keep repeating
+    });
+
